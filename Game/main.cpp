4dbc4156b700ebf57e863271de872b6b857ef454
BowinYem <yemb9@DESKTOP-VB8C2VI>
2024-04-30 21:15:30 -0400

    Playing around with textures and the renderer

--- Game/main.cpp
+++ Game/main.cpp
@@ -1,11 +1,29 @@
 #pragma once
-#include <SDL3/SDL.h>
+#include <SDL.h>
+#include <SDL_image.h>
 
 int main(int argc, char* args[])
 {
 	SDL_Init(SDL_INIT_VIDEO);
 
-	SDL_Window* myWindow = SDL_CreateWindow("TestWindow", 480, 640, SDL_WINDOW_MAXIMIZED);
-	SDL_Surface* WindowSurface = SDL_GetWindowSurface(myWindow);
+	SDL_Window* myWindow = SDL_CreateWindow("TestWindow", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0);
+	SDL_Renderer* TestRenderer = SDL_CreateRenderer(myWindow, -1, SDL_RENDERER_ACCELERATED);
+	SDL_SetRenderDrawColor(TestRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+
+	SDL_Surface* TestSurface = IMG_Load("star.bmp");
+	SDL_Texture* TestTexture = SDL_CreateTextureFromSurface(TestRenderer, TestSurface);
+	SDL_FreeSurface(TestSurface);
+	TestSurface = nullptr;
+	SDL_Rect SrcRect = { 0, 0, 40, 40 };
+	SDL_Rect DestRect = { 300, 300, 40, 40 };
+
+	while (true)
+	{
+		SDL_RenderClear(TestRenderer);
+		SDL_RenderCopy(TestRenderer, TestTexture, &SrcRect, &DestRect);
+		SDL_RenderPresent(TestRenderer);
+	}
+	
+
 	return 0;
 }
