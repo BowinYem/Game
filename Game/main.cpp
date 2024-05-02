69c96ebd71d88df9aa83c22c7bbedd84bc7f9ac6
BowinYem <yemb9@DESKTOP-VB8C2VI>
2024-05-02 19:29:50 -0400

    Implemented basic walk cycle

--- Game/main.cpp
+++ Game/main.cpp
@@ -5,19 +5,20 @@
 #include <vector>
 
 
-static const char SPRITE_WIDTH = 40;
-static const char SPRITE_HEIGHT = 40;
+static const char SPRITE_WIDTH = 30;
+static const char SPRITE_HEIGHT = 31;
+static const char TOTAL_WALK_CYCLE_FRAMES = 3;
+
 
 bool quit = false;
-std::vector<SDL_Rect> SheetRects(4);
+std::vector<SDL_Rect> WallCycleFrames(3);
 
 
 void loadSpriteSheet()
 {
-	SheetRects[0] = { 0, 0, SPRITE_WIDTH, SPRITE_HEIGHT };
-	SheetRects[1] = { 40, 0, SPRITE_WIDTH, SPRITE_HEIGHT };
-	SheetRects[2] = { 80, 0, SPRITE_WIDTH, SPRITE_HEIGHT };
-	SheetRects[3] = { 120, 0, SPRITE_WIDTH, SPRITE_HEIGHT };
+	WallCycleFrames[0] = { 0, 0, SPRITE_WIDTH, SPRITE_HEIGHT };
+	WallCycleFrames[1] = { 30, 0, SPRITE_WIDTH, SPRITE_HEIGHT };
+	WallCycleFrames[2] = { 60, 0, SPRITE_WIDTH, SPRITE_HEIGHT };
 }
 
 int main(int argc, char* args[])
@@ -25,22 +26,23 @@ int main(int argc, char* args[])
 	SDL_Init(SDL_INIT_VIDEO);
 
 	SDL_Window* myWindow = SDL_CreateWindow("TestWindow", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0);
-	SDL_Renderer* TestRenderer = SDL_CreateRenderer(myWindow, -1, SDL_RENDERER_ACCELERATED);
+	SDL_Renderer* TestRenderer = SDL_CreateRenderer(myWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC); // use bitwise OR operand to pass multiple flags 
 	SDL_SetRenderDrawColor(TestRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
 
 	// Loading a texture
-	SDL_Surface* TestSurface = IMG_Load("count.bmp");
-	SDL_SetColorKey(TestSurface, SDL_TRUE, SDL_MapRGB(TestSurface->format, 115, 251, 253));
+	SDL_Surface* TestSurface = IMG_Load("megaman.bmp");
+	SDL_SetColorKey(TestSurface, SDL_TRUE, SDL_MapRGB(TestSurface->format, 128, 0, 128));
 	SDL_Texture* TestTexture = SDL_CreateTextureFromSurface(TestRenderer, TestSurface);
 	SDL_FreeSurface(TestSurface);
 	TestSurface = nullptr;
 	loadSpriteSheet();
-	SDL_Rect SrcRect = { 120, 0, 40, 40 };
-	SDL_Rect DestRect = { 300, 300, 40, 40 };
-	SDL_Rect currentSprite = SheetRects[0];
+	SDL_Rect DestRect = { 300, 300, 100, 100 };
+	SDL_Rect currentSprite = WallCycleFrames[0];
 
 	SDL_Event e;
 	
+	int currentFrame = 0;
+
 	while (!quit)
 	{
 		while(SDL_PollEvent(&e))
@@ -54,21 +56,16 @@ int main(int argc, char* args[])
 		const Uint8* KeyBoardState = nullptr;
 		KeyBoardState = SDL_GetKeyboardState(nullptr);
 
-		if (KeyBoardState[SDL_SCANCODE_LEFT])
+		//if (KeyBoardState[SDL_SCANCODE_LEFT])
+		//{
+
+		//}
+	
+		currentSprite = WallCycleFrames[currentFrame / (TOTAL_WALK_CYCLE_FRAMES * 3)];
+		++currentFrame;
+		if((currentFrame / (TOTAL_WALK_CYCLE_FRAMES * 3)) >= TOTAL_WALK_CYCLE_FRAMES)
 		{
-			currentSprite = SheetRects[0];
-		}
-		if (KeyBoardState[SDL_SCANCODE_UP])
-		{
-			currentSprite = SheetRects[1];
-		}
-		if (KeyBoardState[SDL_SCANCODE_RIGHT])
-		{
-			currentSprite = SheetRects[2];
-		}
-		if (KeyBoardState[SDL_SCANCODE_DOWN])
-		{
-			currentSprite = SheetRects[3];
+			currentFrame = 0;
 		}
 
 		SDL_RenderClear(TestRenderer);
