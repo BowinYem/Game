83512ff5f90d32299c9a627e964726b0e3c66fed
BowinYem <114599912+bowinyemff@users.noreply.github.com>
2024-08-08 15:01:25 -0400

    Commented out collision code

--- Game/GameSystems.cpp
+++ Game/GameSystems.cpp
@@ -52,18 +52,18 @@ bool GameSystems::GameSystems_Close()
 
 void GameSystems::GameSystems_UpdateCollision()
 {
-        for(uint8_t j = 0; j < ProjectilePoolSize; ++j)
-        {
-            if(meteorInUse[i] && GameSystems::projectilePool->IsProjectileInUse(j))
-            {  
-                auto& projectile = GameSystems::projectilePool->GetProjectile(j);
-                bool collided = SDL_HasIntersection(&meteors[i].GetCollisionBox(), &projectile.GetCollisionBox());
+        // for(uint8_t j = 0; j < ProjectilePoolSize; ++j)
+        // {
+        //     if(meteorInUse[i] && GameSystems::projectilePool->IsProjectileInUse(j))
+        //     {  
+        //         auto& projectile = GameSystems::projectilePool->GetProjectile(j);
+        //         bool collided = SDL_HasIntersection(&meteors[i].GetCollisionBox(), &projectile.GetCollisionBox());
 
-                if (collided)
-                {
-                    std::cout << "Collision" << std::endl;
-                    Destroy(i);
-                }
-            }
-        }
+        //         if (collided)
+        //         {
+        //             std::cout << "Collision" << std::endl;
+        //             Destroy(i);
+        //         }
+        //     }
+        // }
 }
