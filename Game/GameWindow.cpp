bd6b265ebbbab6dc1085b322490e6756ae90bd12
bowinyem <114599912+bowinyemff@users.noreply.github.com>
2026-07-11 16:25:19 -0400

    GameWindow - switched parameter and member declaration positions of window width and height

--- Game/GameWindow.cpp
+++ Game/GameWindow.cpp
@@ -1,6 +1,6 @@
 #include "GameWindow.h"
 
-GameWindow::GameWindow(uint16_t window_h, uint16_t window_w) : windowHeight(window_h), windowWidth(window_w)
+GameWindow::GameWindow(uint16_t window_w, uint16_t window_h) : windowWidth(window_w), windowHeight(window_h)
 {
     windowSDLPtr = SDL_CreateWindow("TestWindow", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, windowWidth, windowHeight, 0);
 }
--- Game/GameWindow.h
+++ Game/GameWindow.h
@@ -8,11 +8,11 @@ friend class GameRenderer;
 
 public:
 	GameWindow() = delete;
-	GameWindow(uint16_t window_h, uint16_t window_w);
+	GameWindow(uint16_t window_w, uint16_t window_h);
 	~GameWindow();
 
 private:
 	SDL_Window* windowSDLPtr;
-	uint16_t windowHeight;
 	uint16_t windowWidth;
+	uint16_t windowHeight;
 };
