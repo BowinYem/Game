3ae8072b873f614a00c2b1f0dd2e3a3ebedba910
bowinyem <114599912+bowinyemff@users.noreply.github.com>
2026-07-17 13:22:07 -0400

    Cleaned Up GameWindow.h/.cpp
    - Cleaned up whitespace formatting
    - Made windowWidth/Height public
    - declared copy/move constructors and assignment operators as deleted
    - Created GameGlobals::CreateWindowFlags

--- Game/GameWindow.cpp
+++ Game/GameWindow.cpp
@@ -1,8 +1,9 @@
 #include "GameWindow.h"
+#include "GameSystems.h"
 
 GameWindow::GameWindow(uint16_t window_w, uint16_t window_h) : windowWidth(window_w), windowHeight(window_h)
 {
-    windowSDLPtr = SDL_CreateWindow("TestWindow", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, windowWidth, windowHeight, 0);
+    windowSDLPtr = SDL_CreateWindow("TestWindow", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, windowWidth, windowHeight, GameGlobals::CreateWindowFlags);
 }
 
 GameWindow::~GameWindow()
--- Game/GameWindow.h
+++ Game/GameWindow.h
@@ -8,11 +8,17 @@ friend class GameRenderer;
 
 public:
 	GameWindow() = delete;
+	GameWindow(const GameWindow&) = delete;
+	GameWindow(GameWindow&&) = delete;
+	GameWindow& operator=(const GameWindow&) = delete;
+	GameWindow& operator=(GameWindow&&) = delete;
+	
 	GameWindow(uint16_t window_w, uint16_t window_h);
 	~GameWindow();
 
-private:
-	SDL_Window* windowSDLPtr;
 	uint16_t windowWidth;
 	uint16_t windowHeight;
+
+private:
+	SDL_Window* windowSDLPtr;
 };
